d69bf5ded3ed02fb07c15998d3035190c60f9c85
michael-doll <MWDoll88@gmail.com>
2019-12-09 18:24:18 -0500

    save work, calculator

--- src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
@@ -5,31 +5,39 @@ package rocks.zipcode.quiz4.fundamentals;
  */
 public class Calculator {
     public static Double squareRoot(Double value) {
-        return null;
+        return Math.sqrt(value);
     }
 
     public static Double square(Double value) {
-        return null;
+        return Math.pow(value,2);
     }
 
     public static Double[] squareRoots(Double... value) {
-        return null;
+        Double[] squareRootArr = new Double[value.length];
+        for (int i = 0; i < value.length; i++) {
+            squareRootArr[i] = squareRoot(value[i]);
+        }
+        return squareRootArr;
     }
 
     public static Double[] squares(Double... values) {
-        return null;
+        Double[] l7s = new Double[values.length];
+        for (int i = 0; i < values.length; i++) {
+            l7s[i] = square(values[i]);
+        }
+        return l7s;
     }
 
     public static Double add(Double value1, Double value2) {
-        return null;
+        return value1 + value2;
     }
 
     public static Double subtract(Double value1, Double value2) {
-        return null;
+        return value1 - value2;
     }
 
 
     public static Double divide(Double divisor, Double dividend) {
-        return null;
+        return divisor/dividend;
     }
 }
