235bcb99c5c5497543264773a205288f4d7a90a1
michael-doll <MWDoll88@gmail.com>
2019-12-09 22:38:00 -0500

    wordCounter

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,25 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WordCounter {
+    List<String> strings;
     public WordCounter(String... strings) {
+       this.strings = Arrays.asList(strings);
     }
 
     public Map<String, Integer> getWordCountMap() {
-        return null;
+        Map<String,Integer> countMap = new HashMap<>();
+        for(String string : strings) {
+            if (!countMap.containsKey(string)){
+                countMap.put(string,1);
+            }else{
+                countMap.put(string,countMap.get(string)+1);
+            }
+        }
+        return countMap;
     }
 }
