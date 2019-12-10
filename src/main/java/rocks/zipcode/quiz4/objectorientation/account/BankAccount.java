8c21a28bf58a1cd6a5049142e29edcba0ddce781
michael-doll <MWDoll88@gmail.com>
2019-12-09 22:08:16 -0500

    save work, object orientation

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -4,7 +4,7 @@ package rocks.zipcode.quiz4.objectorientation.account;
  * @author leon on 27/12/2018.
  */
 public class BankAccount extends Account implements Transactable{
-    private Double balance;
+    private Double balance = 0.;
 
     public BankAccount(){}
 
--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
@@ -3,18 +3,59 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 30/12/2018.
  */
-public class Employee {
+public class Employee implements Worker, Transactable{
+
+    private Double numberOfHours = 0.0;
+    private Double hourlyWage = 35.;
+    private BankAccount bankAccount = new BankAccount();
+
     public Employee() {
     }
 
     public Employee(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
     }
 
     public BankAccount getBankAccount() {
-        return null;
+        return bankAccount;
     }
 
     public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
 
+    @Override
+    public void increaseHoursWorked(Double numberOfHours) {
+        this.numberOfHours += numberOfHours;
+    }
+
+    @Override
+    public Double getHoursWorked() {
+        return numberOfHours;
+    }
+
+    @Override
+    public Double getHourlyWage() {
+        return hourlyWage;
+    }
+
+    @Override
+    public Double getMoneyEarned() {
+        return hourlyWage * numberOfHours;
+    }
+
+    @Override
+    public void deposit(Double amountToIncreaseBy) {
+        bankAccount.deposit(amountToIncreaseBy);
+    }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) {
+        bankAccount.withdrawal(amountToDecreaseBy);
+    }
+
+    @Override
+    public Double getBalance() {
+        return bankAccount.getBalance();
     }
 }
